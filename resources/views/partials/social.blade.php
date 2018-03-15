54408a70d4fba8b0782862e17e52d12b7d8c0ca4
Thanh Phong <14110143@student.hcmute.edu.vn>
2018-03-15 22:03:53 +0700

    fix social media sticky head

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,11 +1,12 @@
 <div class="social-bar hidden-sm" id="mySidenav">
-	<a href="" class="icon icon-facebook" target="_blank">
+	<a href="https://www.facebook.com/kelvinphong168" class="icon icon-facebook" target="_blank"
+	title="facebook">
 		<img src="assets/img/facebook.png" alt=""></a>
-	<a href="assets/img/facebook.png" class="icon icon-twitter" target="_blank">
+	<a href="#" class="icon icon-twitter" target="_blank" title="facebook">
 		<img src="assets/img/twitter.png" alt=""></a>
 	</a>
-	<a href="assets/img/facebook.png" class="icon icon-google" target="_blank">
+	<a href="#" class="icon icon-google" target="_blank" title="facebook">
 		<img src="assets/img/google-plus.png" alt=""></a>
-	<a href="assets/img/linkedin.png" class="icon icon-linkedin" target="_blank">
+	<a href="#" class="icon icon-linkedin" target="_blank" title="facebook">
 		<img src="assets/img/linkedin.png" alt=""></a>
   </div>
